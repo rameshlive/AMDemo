673755aa6e083fb5a3cad7ea5ae6c63475919bf8
rameshlive <ramesh.canny@gmail.com>
2020-04-19 17:13:55 +0530

    ngb carousel integration on home pgae

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -23,9 +23,9 @@ export class ProductComponent implements OnInit {
   addToWishlist(id:string,productname: string) {
     this._CartService.addToWishlist(this.product);
     this.selectedItem = id;
-    productname = `The product ${productname} addedd to your wishlist!`;
+    productname = `${productname} addedd to your wishlist!`;
     this._wishlistSnack.open(productname, "Close", {
-      duration: 20000000,
+      duration: 2000,
       verticalPosition: 'top'
     });
   }
